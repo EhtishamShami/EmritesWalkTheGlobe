f89685877ba9b9b1986437defebaa33989aa0956
Shami <theehtishamshami@live.com>
2017-01-17 15:28:33 +0500

    yo yo

--- app/src/main/java/com/example/abubakernazir/emirateswalktheglobe/AppClientListerner.java
+++
@@ -1,9 +0,0 @@
-package com.example.abubakernazir.emirateswalktheglobe;
-
-//import org.json..parser.JSONParser;
-public interface AppClientListerner{
-
-
-    public void callback(String result,String ServiceName);
-
-}
